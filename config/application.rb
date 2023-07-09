421481ef6345544e920daa5f140b39d3245a8178
fgolfc <cf.ts.fujimoto@gmail.com>
2023-07-09 22:51:04 +0900

    node upgrade

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ module DeviseTest
   class Application < Rails::Application
     config.load_defaults 6.1
     config.enable_dependency_loading = true
+    config.assets.initialize_on_precompile = false # ここに追加
     config.i18n.default_locale = :ja
   end
 end
+
