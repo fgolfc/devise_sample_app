0216bca4fd9f1c9b6577725c4a2bdfdf2dcc7db1
fgolfc <cf.ts.fujimoto@gmail.com>
2023-07-11 10:52:12 +0900

    5th commit

--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,7 @@ ActiveRecord::Schema.define(version: 2023_05_04_005720) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.boolean "admin", default: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
