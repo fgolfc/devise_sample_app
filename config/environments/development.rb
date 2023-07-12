b23166d3ad871a94be3adcbb3f1a8739c2a4f542
fgolfc <cf.ts.fujimoto@gmail.com>
2023-07-12 13:23:43 +0900

    development.rb change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,19 +40,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :domain => ENV['HOST'],
-    :user_name => ENV['SENDGRID_USER_NAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
