3ff3c02bd1e938350554f4f1411632f7e51aacb5
fgolfc <cf.ts.fujimoto@gmail.com>
2023-07-15 12:49:40 +0900

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users', to: redirect("/users/sign_up")
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   root 'blogs#index'
   resources :blogs
   if Rails.env.development?
